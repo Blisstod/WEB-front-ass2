1ca2834d97e07ab0ad5dd9aac0eb72fd5d7fde3b
azatich <azatabdirashituly@gmail.com>
2023-10-31 20:07:34 +0600

    added login and registration.js

---
+++ js/login.js
@@ -0,0 +1,37 @@
+const login = document.getElementById("loginBtn")
+
+login.addEventListener('click', function(event) {
+    event.preventDefault();
+    
+    const username = document.getElementById("logUsername").value
+    const password = document.getElementById("logPassword").value
+
+    if (username in localStorage) {
+        let user = localStorage.getItem(username)
+        user = JSON.parse(user)
+        let userPass = user['password']
+        if (password == "") {
+            alert("type your password")
+        } else if (password == userPass){
+            alert("Logged in")
+            window.location.href = "./index.html";
+        } else {
+            alert("Password incorrect")
+        }
+    } else {
+        alert("Please, register")
+    }
+})
+
+
+let eyeicon = document.getElementById("eyeicon")
+let passw = document.getElementById("logPassword")
+eyeicon.onclick = function() {
+    if (passw.type == "password") {
+        passw.type = "text"
+        eyeicon.name = "eye-outline"
+    } else {
+        passw.type = "password"
+        eyeicon.name = "eye-off-outline"
+    }
+}
